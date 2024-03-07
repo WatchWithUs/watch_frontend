7c26109fc6ef70a1a6c02f3fec1d9e97c42176ac
João Dias <152170826+jvfd1983@users.noreply.github.com>
2024-03-07 11:34:23 +0000

    Search bar in NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import axios from 'axios';
 
 function Navbar() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await axios.get(`/api/movies/search?q=${searchTerm}`);
+      setSearchResults(response.data.movies);
+    } catch (error) {
+      console.error('Erro ao pesquisar filmes:', error);
+    }
+  };
+
   return (
-    <nav>
+    <nav className="navbar">
       <Link to="/">
         <button>Home</button>
       </Link>
@@ -14,6 +35,27 @@ function Navbar() {
       <Link to="/collection/create">
         <button>Create Collection</button>
       </Link>
+
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {searchResults.length > 0 && (
+        <div className="search-results">
+          <h3>Search Results</h3>
+          <ul>
+            {searchResults.map(movie => (
+              <li key={movie.id}>{movie.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
