ebd2cfb9198b829a89cb2e8d74cd8790da7e6032
João Dias <152170826+jvfd1983@users.noreply.github.com>
2024-03-11 14:17:22 +0000

    Code Updated

--- src/pages/CollectionListPage.jsx
+++ src/pages/CollectionListPage.jsx
@@ -4,7 +4,6 @@ import CollectionList from '../components/CollectionList';
 function CollectionListPage() {
   return (
     <div>
-      <h1>Collection List</h1>
       <CollectionList />
     </div>
   );
