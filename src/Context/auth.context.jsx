4858ac38b9950c661dd53759329bb771fd0d3543
João Dias <152170826+jvfd1983@users.noreply.github.com>
2024-03-12 22:38:39 +0000

    auth.context updated

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,55 +1,60 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const API_URL = import.meta.env.VITE_API_URL;
 const AuthContext = React.createContext();
-function AuthProviderWrapper(props){
+
+function AuthProviderWrapper(props) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [user, setUser] = useState(null);
+
     const getToken = () => localStorage.getItem('authToken');
-    /* Store the token in the local storage */
-    const storeToken = (token) =>{
+
+    const storeToken = (token) => {
         localStorage.setItem('authToken', token);
     }
-    const removeToken = () =>{
+
+    const removeToken = () => {
         localStorage.removeItem('authToken');
     }
-    /* Authenticate the User via JWT */
+
     const authenticateUser = () => {
         const storedToken = localStorage.getItem('authToken');
         if (storedToken) {
-           axios.get(`${API_URL}/auth/verify`, { headers: { Authorization: `Bearer ${storedToken}` } })
-           //axios.get(`${API_URL}/auth/verify`)
+            axios.get(`${API_URL}/auth/verify`, { headers: { Authorization: `Bearer ${storedToken}` } })
                 .then((response) => {
                     const user = response.data;
                     setIsLoggedIn(true);
                     setUser(user);
                     setIsLoading(false);
                 })
-            .catch(()=>{
-                setIsLoggedIn(false);
-                setIsLoading(true);
-                setUser(null);
-            })
-        }
-        else {
+                .catch(() => {
+                    setIsLoggedIn(false);
+                    setIsLoading(true);
+                    setUser(null);
+                })
+        } else {
             setIsLoggedIn(false);
             setIsLoading(true);
             setUser(null);
         }
     }
-    const logOut = () =>{
+
+    const logOut = () => {
         removeToken();
         authenticateUser();
     }
-    useEffect(()=>{
+
+    useEffect(() => {
         authenticateUser();
     }, []);
-    return(
-         <AuthContext.Provider value={{isLoggedIn, isLoading, user, setUser, storeToken, authenticateUser, logOut, getToken}}>
-             {props.children}
-         </AuthContext.Provider>
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, isLoading, user, setUser, storeToken, authenticateUser, logOut, getToken }}>
+            {props.children}
+        </AuthContext.Provider>
     )
 }
- //export {AuthProviderWrapper, AuthContext, logOut};
- export {AuthProviderWrapper, AuthContext}
+
+export { AuthProviderWrapper, AuthContext };
