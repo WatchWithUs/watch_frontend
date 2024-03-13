2f3a1346079a14072777ba7ecced0dc25e2447ff
João Dias <152170826+jvfd1983@users.noreply.github.com>
2024-03-13 14:43:14 +0000

    Updated CollectionPage add and delete working

--- src/pages/UpdateCollectionPage.jsx
+++ src/pages/UpdateCollectionPage.jsx
@@ -10,11 +10,13 @@ function UpdateCollectionPage() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [movies, setMovies] = useState([]);
-  const [newMovie, setNewMovie] = useState("");
+  const [selectedMovies, setSelectedMovies] = useState([]); // Lista de filmes selecionados
+  const [availableMovies, setAvailableMovies] = useState([]); // Lista de filmes disponíveis para adição
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchCollection();
+    fetchAvailableMovies();
   }, []);
 
   const fetchCollection = async () => {
@@ -24,9 +26,17 @@ function UpdateCollectionPage() {
       setTitle(response.data.title);
       setDescription(response.data.description);
       setMovies(response.data.movies);
-      console.log('id: ',id);
     } catch (error) {
       console.error('Error fetching collection:', error);
+    }
+  };
+
+  const fetchAvailableMovies = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/movies`);
+      setAvailableMovies(response.data);
+    } catch (error) {
+      console.error('Error fetching available movies:', error);
     }
   };
 
@@ -36,7 +46,6 @@ function UpdateCollectionPage() {
       await axios.put(`${API_URL}/collection/${id}`, { title, description, selectedMovies: movies });
       setIsLoading(false);
       window.location.href = "/collectionList";
-      console.log('movies: ',movies)
     } catch (error) {
       setIsLoading(false);
       console.error('Error updating collection:', error);
@@ -49,13 +58,11 @@ function UpdateCollectionPage() {
     console.log('Movie deleted successfully');
   };
 
-  const handleAddMovie = () => {
-    if (newMovie.trim() !== "") {
-      const updatedMovies = [...movies, { title: newMovie }];
-      setMovies(updatedMovies);
-      setNewMovie(""); // Reset input field after adding movie
-      console.log('Movie added successfully');
-    }
+  const handleAddMovies = () => {
+    const updatedMovies = [...movies, ...selectedMovies]; // Adicionando todos os filmes selecionados
+    setMovies(updatedMovies);
+    setSelectedMovies([]); // Limpa a lista de filmes selecionados após a adição
+    console.log('Movies added successfully');
   };
 
   return (
@@ -79,14 +86,21 @@ function UpdateCollectionPage() {
             </li>
           ))}
         </ul>
-        <div>
-          <input type="text" value={newMovie} onChange={(e) => setNewMovie(e.target.value)} />
-          <button onClick={handleAddMovie}>Add Movie</button>
-        </div>
       </div>
-      <button onClick={handleUpdateCollection} disabled={isLoading}>
-        {isLoading ? 'Updating...' : 'Update'}
-      </button>
+      <div>
+        <h4>Select Movies to Add:</h4>
+        <select multiple value={selectedMovies} onChange={(e) => setSelectedMovies([...e.target.selectedOptions].map(option => option.value))}>
+          {availableMovies.map((movie, index) => (
+            <option key={index} value={movie._id}>{movie.title}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <button onClick={handleAddMovies}>Add Movies</button>
+        <button onClick={handleUpdateCollection} disabled={isLoading}>
+          {isLoading ? 'Updating...' : 'Update'}
+        </button>
+      </div>
     </div>
   );
 }
