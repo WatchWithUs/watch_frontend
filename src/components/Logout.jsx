43fc409cf4cb7cdaa63ac82e817b8e649b8aecdc
Aron <emmanuel.aron@gmail.com>
2024-03-13 10:57:43 +0100

    add Logout component

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {logOut} from '../Context/auth.context'
+
 function Logout() {
+
   return (
     <button onClick={logOut}>Logout</button>
   );
 }
+
 export default Logout;
